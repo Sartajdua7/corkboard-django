b707f1f92acab55f6099f5237c260c31dcea9b69
Sartajdua7 <38698048+Sartajdua7@users.noreply.github.com>
2023-02-24 21:03:00 -0800

    Update views.py

--- corkboard/views.py
+++ corkboard/views.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 def index(request, latitude, longitude):
-    gmaps = googlemaps.Client(key='AIzaSyBYm9_ggjcyAUFBLBhr8fV79fvNpXUoqOw')
+    gmaps = googlemaps.Client(key='')
     nearby_restaurants = gmaps.places_nearby(location=(latitude, longitude), radius=10000, language='english',
                                              type='restaurant')
     return HttpResponse(nearby_restaurants['results'])
